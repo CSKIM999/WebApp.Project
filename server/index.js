ec5288abaed25961cd396cb21170224d9899a96c
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2022-09-14 21:11:00 +0900

    22/09/14 update for ssl
    21:10 from desktop

--- server/index.js
+++ server/index.js
@@ -6,6 +6,21 @@ const config = require("./config/key");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+if (process.env.MONGO_URI) {
+  const options = {
+    ca: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem`),
+    key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem`),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN_NAME}/cert.pem`),
+  };
+  https.createServer(options, app).listen(443, () => {
+    console.log("443번 포트에서 대기중입니다.");
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`DEV MODE ON! App listening on port ${port}!`);
+  });
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -23,4 +38,4 @@ app.get("/", (req, res) => {
   return res.send("hello~~ cskim speaking in ubuntu");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
