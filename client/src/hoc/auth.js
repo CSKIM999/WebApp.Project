7c60f71e06afe19287bacb7f595da2b3a0da748c
CSKIM <rlackstjq987@gmail.com>
2024-01-12 21:54:23 +0900

    Update auth.js

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -36,6 +36,7 @@ export default function AUTH(SpecificComponent, option, adminRoute = null) {
           }
         }
       });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return <SpecificComponent {...props} user={user} />;
